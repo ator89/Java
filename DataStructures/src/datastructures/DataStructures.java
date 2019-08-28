1d27aa1eb0c537b11ef86b5bdb85a81f951bf022
antox89 <ator89@outlook.com>
2019-08-28 10:40:54 -0600

    ADT

---
+++ DataStructures/src/datastructures/DataStructures.java
@@ -0,0 +1,17 @@
+
+package datastructures;
+
+/**
+ *
+ * @author angel
+ */
+public class DataStructures {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
