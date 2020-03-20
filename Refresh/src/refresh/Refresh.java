8e2e0efb6a8e26506f49b69620be170d69d4c98e
antox89 <ator89@outlook.com>
2020-03-20 10:51:04 -0600

    Archives

---
+++ Refresh/src/refresh/Refresh.java
@@ -0,0 +1,30 @@
+
+package refresh;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ *
+ * @author angel
+ */
+public class Refresh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
+
+class readFile{
+    
+    public void read() throws FileNotFoundException{
+        
+        FileReader input = new FileReader("file.text");
+    }
+}
